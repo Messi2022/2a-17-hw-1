0952e463b56c3c830eebfa229c1a62fa53c762a8
Messi2022 <99188396+Messi2022@users.noreply.github.com>
2022-02-07 21:11:35 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("2A")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Cliff")
+})
